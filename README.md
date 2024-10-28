4ed63aa95e765da1a7669ea33f8b9c1f05a0b557
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-10-28 19:28:41 +0100

    print session id and set admin username and password in application properties

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ App to learn how to implement Spring Boot Security
 
 Parts:
 - Default login form
-- 
+- SessionID
+- username and password in application properties
--- src/main/java/com/dba/Spring_Security/controller/HelloController.java
+++ src/main/java/com/dba/Spring_Security/controller/HelloController.java
@@ -1,14 +1,17 @@
 package com.dba.Spring_Security.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HelloController {
 
     @GetMapping("hello")
-    public String greet() {
-        return "Hello World!";
+    public String greet(HttpServletRequest req) {
+        return "Hello World! " + req.getSession().getId();
     }
 
 }
